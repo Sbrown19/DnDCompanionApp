9f2cf64c40f7ee6e6e53e1f7fa5fbd7a056a7cd3
NicholasLanes <97725017+NicholasLanes@users.noreply.github.com>
2022-02-12 23:57:55 -0600

    On To Bootstrap
    ready to incorporate bootstrap and entity framework //TODO

--- DnDCompanionWebApp/Models/LoginModel.cs
+++ DnDCompanionWebApp/Models/LoginModel.cs
@@ -1,10 +1,14 @@
-﻿namespace DnDCompanionWebApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DnDCompanionWebApp.Models
 {
     public class LoginModel
     {
+        [Required(ErrorMessage = "Please enter a username to proceed.")]
         public string UserName { get; set; }
+        [Required(ErrorMessage = "Please enter a password to proceed.")]
         public string Password { get; set; }
-        public bool RememberMe { get; set; } = false;
+        public bool IsDungeonMaster { get; set; }
 
     }
 }
