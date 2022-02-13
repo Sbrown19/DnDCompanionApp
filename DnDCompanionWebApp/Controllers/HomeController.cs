845ce580d1b0ab20350a1bff9340136af8d399c4
NicholasLanes <97725017+NicholasLanes@users.noreply.github.com>
2022-02-12 23:16:18 -0600

    Initial Commit
    First commit. The project is mostly bones right now. Working on the index view at the moment.

---
+++ DnDCompanionWebApp/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using DnDCompanionWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DnDCompanionWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        //Home controller index displays the login page
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.Title = "Home";
+            ViewBag.Description = "DnD Companion Application Log-In Screen";
+            return View();
+        }
+        //Returns view results dependent on user's login selection
+        [HttpPost]
+        public IActionResult Index(LoginModel loginResult)
+        {
+            return View(loginResult);
+        }
+    }
+}
---
+++ DnDCompanionWebApp/Models/LoginModel.cs
@@ -0,0 +1,10 @@
+﻿namespace DnDCompanionWebApp.Models
+{
+    public class LoginModel
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public bool RememberMe { get; set; } = false;
+
+    }
+}
