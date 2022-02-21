db298de387272d84ff026f8faa3bfe86c5d26ebc
NicholasLanes <97725017+NicholasLanes@users.noreply.github.com>
2022-02-21 14:56:03 -0600

    Module6Assignment
    Created a dropdown menu item under the Assignment dropdown called "Assignment 6.1" using Bootstrap. Created a new EMPTY controller for the assignment. Created a new EMPTY razor view for Students (for the assignment).
    //TODO
    student model class
    route parameters for access level
    view model with student objects that takes access level
    views for access level results
    list with 3-5 students

---
+++ DnDCompanionWebApp/Controllers/Module6AssignmentController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DnDCompanionWebApp.Controllers
+{
+    public class Module6AssignmentController : Controller
+    {
+        [HttpGet]
+        [Route("[controller]/[action]/{id?}")]
+        public IActionResult Students()
+        {
+            ViewBag.Title = "Students";
+            ViewBag.Description = "Assignment 6.1 Home Page";
+            return View();
+        }
+    }
+}
