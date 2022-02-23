e2365dfecd9c187b0f428ab5f928b10302c74f92
JJosephK <jjkrebs1@dmacc.edu>
2022-02-22 22:07:26 -0600

    Added Student model and view model, added index link for view, added view

---
+++ DnDCompanionWebApp/Models/StudentModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DnDCompanionWebApp.Models
+{
+    public class StudentModel
+    {
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Grade { get; set; }
+
+        public StudentModel()
+        {
+
+        }
+
+        public StudentModel(string firstName)
+        {
+            FirstName = FirstName;
+        }
+
+        public StudentModel(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+
+        }
+
+        public StudentModel(string firstName, string lastName, string grade)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Grade = grade;
+
+        }
+
+
+    }
+}
